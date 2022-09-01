0bdfc27d98c1858b9d3cf9c858b56e3debf53c9f
pulk66-s <hugo.marrasse@epitech.eu>
2022-09-01 14:48:58 +0200

    [ADD] Project Euler problem 5

--- eulerProject/problem6.r
+++ eulerProject/problem6.r
@@ -1,26 +1,3 @@
-isPrime <- function(n) {
-    if (n == 2) {
-        return(TRUE)
-    }
-    if (n %% 2 == 0) {
-        return(FALSE)
-    }
-    for (i in 2:(n - 1)) {
-        if (n %% i == 0) {
-            return(FALSE)
-        }
-    }
-    return(TRUE)
-}
-
-index <- 1
-nb <- 2
-
-while (index < 10001) {
-    nb <- nb + 1
-    if (isPrime(nb)) {
-        index <- index + 1
-    }
-}
-print("==END==")
-print(nb)
+nb <- 1:100
+sum <- sum(nb)
+print(sum^2 - sum(nb^2))
