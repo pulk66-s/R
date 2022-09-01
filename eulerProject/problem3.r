d9b8736055484d415f3270c55c449fbd6ae13b6c
pulk66-s <hugo.marrasse@epitech.eu>
2022-09-01 14:18:08 +0200

    [ADD] Project euler problem 3

--- eulerProject/problem3.r
+++ eulerProject/problem3.r
@@ -1,11 +1,33 @@
-# Algorithme d'euclide
-PGCD <- function(a, b) {
-    while (b != 0) {
-        t <- b
-        b <- a %% b
-        a <- t
+isPrime <- function(n) {
+    if (n == 2) {
+        return(TRUE)
     }
-    return (a)
+    if (n %% 2 == 0) {
+        return(FALSE)
+    }
+    i <- 3
+    while (i <= n - 1) {
+        if (n %% i == 0) {
+            return (FALSE)
+        }
+        i <- i + 2
+    }
+    return(TRUE)
 }
 
-print(PGCD(10, 15))
+nb <- 600851475143
+div <- 2
+factors <- c()
+
+while (nb > 1) {
+    while (!isPrime(div)) {
+        div <- div + 1
+    }
+    while (nb %% div == 0) {
+        factors <- c(factors, div)
+        nb <- nb / div
+    }
+    div <- div + 1
+}
+print(factors)
+print(max(factors))
